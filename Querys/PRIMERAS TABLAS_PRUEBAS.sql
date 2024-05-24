25e638eb7e8262e0073ce835bdf653cf38ff0f5c
Fernando Elian Benitez <febenitez@frba.utn.edu.ar>
2024-05-24 15:37:46 -0300

    Cree las primeras tablas, falta pero me estoy fijando si esta bien

---
+++ Querys/PRIMERAS TABLAS_PRUEBAS.sql
@@ -0,0 +1,68 @@
+USE GD1C2024
+
+-- TICKET
+CREATE TABLE Ticket (
+	ticket_numero SMALLINT IDENTITY PRIMARY KEY,
+	legajo_empleado SMALLINT FOREIGN KEY REFERENCES Empleado(legajo_empleado),
+	id_sucursal SMALLINT FOREIGN KEY REFERENCES Sucursal(id_sucursal),
+	--nro_envio SMALLINT FOREIGN KEY REFERENCES Envio(nro_envio),
+	--nro_caja SMALLINT FOREIGN KEY REFERENCES Caja(nro_caja),
+	ticket_fecha_hora DATETIME NOT NULL,
+	ticket_tipo_comprobante VARCHAR(10) NOT NULL,
+	ticket_total_descuento_aplicado DECIMAL(18,2) DEFAULT 0, 
+	ticket_total_descuento_aplicado_mp DECIMAL(18,2) DEFAULT 0,
+	ticket_subtotal_productos DECIMAL(18,2),
+	ticket_total_ticket DECIMAL(18,2)
+)
+
+-- EMPLEADO
+CREATE TABLE Empleado (
+	legajo_empleado SMALLINT IDENTITY PRIMARY KEY,
+	empleado_nombre NVARCHAR(255),
+	empleado_apellido NVARCHAR(255),
+	empleado_dni INT,
+	empleado_fecha_registro DATETIME NOT NULL,
+	empleado_telefono VARCHAR(20),
+	empleado_mail NVARCHAR(100),
+	empleado_fecha_nacimiento DATE NOT NULL
+)
+
+
+CREATE TABLE Sucursal (
+	id_sucursal SMALLINT PRIMARY KEY,
+	id_super SMALLINT FOREIGN KEY REFERENCES Supermercado(id_super),
+	id_domicilio SMALLINT FOREIGN KEY REFERENCES Domicilio(id_domicilio),
+	sucursal_nombre VARCHAR(50)
+)
+
+CREATE TABLE Supermercado (
+	id_super SMALLINT PRIMARY KEY,
+	id_domicilio SMALLINT FOREIGN KEY REFERENCES Domicilio(id_domicilio),
+	super_nombre NVARCHAR(50),
+	super_razon_soc NVARCHAR(50),
+	super_cuit VARCHAR(25),
+	super_IIBB NVARCHAR(50),
+	super_fecha_ini_actividad DATETIME NOT NULL,
+	super_condicion_fiscal NVARCHAR(255)
+)
+
+CREATE TABLE Domicilio (
+	id_domicilio SMALLINT PRIMARY KEY,
+	id_localidad SMALLINT FOREIGN KEY REFERENCES Localidad(id_localidad),
+	direccion NVARCHAR(50) not null,
+	altura INT not null,
+	piso SMALLINT,
+	departamento SMALLINT
+)
+
+CREATE TABLE Localidad (
+	id_localidad SMALLINT PRIMARY KEY,
+	id_provincia SMALLINT FOREIGN KEY REFERENCES Provincia(id_provincia),
+	nombre_localidad NVARCHAR(50),
+	codigo_postal SMALLINT
+)
+
+CREATE TABLE Provincia (
+	id_provincia SMALLINT PRIMARY KEY,
+	nombre_provincia NVARCHAR(50)
+)
